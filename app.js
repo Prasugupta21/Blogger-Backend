32eb236e698e2029dc6b4f15426cc43a9dae98ca
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-07-30 13:19:12 +0530

    update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express=require("express");
-
+const path =require('path');
 const bodyParser=require("body-parser");
 
 
@@ -11,19 +11,13 @@ const postRoute=require('./routes/post')
 const commentRoute=require('./routes/comment');
 
 const connectDB=require('./connection/db');
-const cors=require('cors');
+
 
 const PORT= 8000;
 
-
 connectDB();
 const app=express();
-const corsOptions = {
-    origin: '*',
-    credentials: true,
-    optionSuccessStatus: 200
-  }
-  app.use(cors(corsOptions))
+
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
@@ -36,7 +30,21 @@ app.use('/',userRoute);
 
 app.use('/posts',postRoute);
 app.use('/comments',commentRoute);
+app.use(express.static(path.join(__dirname, '/client/build')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
 app.listen(PORT,()=>{
     console.log(`server started on port:${PORT}` )
 })
