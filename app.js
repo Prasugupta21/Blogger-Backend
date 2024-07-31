a89f28413066c8e1a265b3c93e81bb0c0caae782
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-07-31 15:47:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const PORT= 8000;
 connectDB();
 const app=express();
 const corsOptions = {
-  origin: '*' , // your frontend URL
+  origin: 'https://blogger-teal-one.vercel.app' , // your frontend URL
   credentials: true,
   optionsSuccessStatus: 200
 };
