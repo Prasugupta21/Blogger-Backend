bc2af169c1de4ee8752c928bd7f5a6554468ff39
Prasugupta21 <101421981+Prasugupta21@users.noreply.github.com>
2024-07-31 23:13:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express=require("express");
-
+const path=require('path');
 const bodyParser=require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors=require('cors');
